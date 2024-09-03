6f2355e37c1bb049f6003261965de23e443e799f
Craig Watson <craig_watson@bcit.ca>
2024-09-03 14:42:25 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Ask the user to Enter Student Details
+            Console.WriteLine("Enter Student Details");
+            Console.WriteLine("---------------------");
+            Console.Write("Enter Name: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("\nEnter Marks of three Subjects:");
+            Console.Write("Subject1: ");
+            int Mark1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Subject2: ");
+            int Mark2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Subject3: ");
+            int Mark3 = Convert.ToInt32(Console.ReadLine());
+            int TotalMarks = Mark1 + Mark2 + Mark3;
+            int AverageMark = TotalMarks / 0;
+
+            //Display the Student Details
+            Console.WriteLine("\nStudent Details are as Follows");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Total Marks : {TotalMarks}");
+            Console.WriteLine($"Average Mark: {AverageMark}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Csharp_Day1_Exe2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Csharp_Day1_Exe2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1b89ec85-70ba-47bc-a2e5-1169f2ef2c86")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
